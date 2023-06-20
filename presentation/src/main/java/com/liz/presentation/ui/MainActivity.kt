36c4bafdf5815c01a0e8f0eba120f0cfd6b8427b
liz <linjeong0208@gmail.com>
2023-06-21 06:06:24 +0900

    메인 화면을 컴포즈로 변경

--- presentation/src/main/java/com/liz/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/liz/presentation/ui/MainActivity.kt
@@ -1,59 +1,35 @@
 package com.liz.presentation.ui
 
 import android.os.Bundle
-import android.view.Menu
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.navigation.NavigationView
 import com.liz.presentation.R
-import com.liz.presentation.databinding.ActivityMainBinding
+import com.liz.presentation.common.enumdata.Route
+import com.liz.presentation.common.ui.drawer.DrawerItemData
+import com.liz.presentation.ui.main.MainUi
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-//        binding.appBarMain.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_search
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        setContent {
+            MainUi(
+                menuItemList = listOf(
+                    DrawerItemData(
+                        Route.SEARCH,
+                        R.string.menu_search,
+                        R.drawable.ic_menu_gallery
+                    ),
+                    DrawerItemData(
+                        Route.TEST,
+                        R.string.menu_test,
+                        R.drawable.ic_launcher_foreground
+                    )
+                )
+            )
+        }
     }
 }
---
+++ presentation/src/main/java/com/liz/presentation/ui/main/MainUi.kt
@@ -0,0 +1,76 @@
+package com.liz.presentation.ui.main
+
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.liz.presentation.R
+import com.liz.presentation.common.enumdata.Route
+import com.liz.presentation.common.ui.drawer.DrawerItemData
+import com.liz.presentation.common.ui.drawer.DrawerUi
+import com.liz.presentation.common.ui.theme.Theme
+import com.liz.presentation.graph.mainGraph
+
+@Preview
+@Composable
+fun MainActivityPreview() {
+    MainUi(
+        menuItemList = listOf(
+            DrawerItemData(
+                Route.SEARCH,
+                R.string.menu_search,
+                R.drawable.ic_menu_gallery
+            )
+        )
+    )
+}
+
+@Composable
+fun MainUi(
+    menuItemList: List<DrawerItemData>,
+    defaultRoute: Route = Route.SEARCH,
+    navController: NavHostController = rememberNavController(),
+    drawerState: DrawerState = rememberDrawerState(initialValue = DrawerValue.Closed),
+) {
+    Theme {
+        Surface {
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    DrawerUi(
+                        menuItemList = menuItemList,
+                        drawerState = drawerState,
+                        defaultRoute = defaultRoute
+                    )
+                    { route ->
+                        setDrawableAction(navController, route)
+                    }
+                }
+            ) {
+                NavHost(
+                    navController,
+                    startDestination = defaultRoute.name
+                ) {
+                    mainGraph(drawerState)
+                }
+            }
+        }
+    }
+}
+
+private fun setDrawableAction(navController: NavHostController, route: Route) {
+    when (route) {
+        Route.SEARCH -> navController.navigate(route.name) {
+            popUpTo(Route.SEARCH.name)
+        }
+        Route.TEST -> navController.navigate(route.name) {
+            popUpTo(Route.TEST.name)
+        }
+    }
+}
